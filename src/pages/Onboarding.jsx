f52b0e0a9bff1182fe1c3304ed4973209705033c
Ajmal Razaq Bhatti <theajmalrazaq@gmail.com>
2025-08-27 01:09:02 +0500

    inital commit

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/logo.svg";
 export default function Splash() {
     return (
-        <div className="bg-black flex py-8 flex-col justify-between items-center h-lvh overflow-hidden">
+        <div className="h-dvh bg-black flex flex-col justify-between items-center px-4 pt-safe-offset-8 pb-safe-offset-8">
             <h1 className="text-white text-xl font-semibold text-center font-playfair">
                 FAST timetable <span className="text-accent">sucks?</span> 
                 <br />
@@ -9,7 +9,7 @@ export default function Splash() {
             </h1>
             <img src={logo} alt="Logo" className="w-50 h-50 user-select-none" />
 
-                <button className="bg-accent font-gilroy text-white px-4 py-2 rounded-full w-[300px] p-2">
+                <button className="bg-accent font-product-sans text-white px-4 py-2 rounded-full w-[300px] text-[16px] p-2">
                     Get Started
                 </button>
             
