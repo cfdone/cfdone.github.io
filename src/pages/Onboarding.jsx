0419f34074ac890fc1c007023d788d322154c308
Ajmal Razaq Bhatti <theajmalrazaq@gmail.com>
2025-08-27 00:47:47 +0500

    inital commit

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,8 +1,18 @@
-import Lottie from "lottie-react";
+import logo from "../assets/logo.svg";
 export default function Splash() {
     return (
-        <div className="bg-black flex justify-center items-center h-screen">
-            <h1 className="text-white text-4xl font-bold"></h1>
+        <div className="bg-black flex py-8 flex-col justify-between items-center h-screen">
+            <h1 className="text-white text-xl font-semibold text-center font-playfair">
+                FAST timetable <span className="text-accent">sucks?</span> 
+                <br />
+               <span className="text-sm">Yeah, we know!</span>
+            </h1>
+            <img src={logo} alt="Logo" className="w-50 h-50 user-select-none" />
+
+                <button className="bg-accent font-gilroy text-white px-4 py-2 rounded-full w-[300px] p-2">
+                    Get Started
+                </button>
+            
         </div>
     );
 }
