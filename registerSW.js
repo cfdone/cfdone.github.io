a64a8a81ad859dd94b6ace4c8dadc0990e3fdf45
Ajmal Razaq Bhatti <theajmalrazaq@gmail.com>
2025-09-08 23:28:54 +0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/cfdone/sw.js', { scope: '/cfdone/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
